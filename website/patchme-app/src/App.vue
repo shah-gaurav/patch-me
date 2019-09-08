e73f17f65c27d09e04c792540e5f86d3cb18c6ac
Gaurav Shah <shahgaurav@yahoo.com>
2019-09-08 14:02:13 -0600

    Added source code for PatchMe.app website

---
+++ website/patchme-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="logo">
+        <span>Patch Me</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text href="https://github.com/shah-gaurav/patch-me" target="_blank">
+        <v-icon>mdi-github-circle</v-icon>&nbsp;
+        <span class="mr-2">Souce Code</span>
+      </v-btn>
+      <v-btn
+        text
+        href="https://github.com/shah-gaurav/patch-me/blob/master/PRIVACY.md"
+        target="_blank"
+      >
+        <span class="mr-2">Privacy Policy</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+
+    <v-footer>
+      <div class="flex-grow-1"></div>
+      <div class="body-2">&copy; <a href="http://www.linkedin.com/in/gaurav-shah-boise" target="_blank">Gaurav Shah</a> - {{ new Date().getFullYear() }}</div>
+      <div class="flex-grow-1"></div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Pacifico&display=swap");
+
+.logo {
+  font-family: "Pacifico", cursive;
+  font-size: 26px;
+}
+
+</style>
---
+++ website/patchme-app/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/patch-me-logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="font-weight-bold mb-3 title">
+          Patch Me: Eye Patch Tracking
+        </h1>
+        <p class="font-weight-regular body-1">
+          Patch Me is an Open Source FREE App that allows parents to track the eye patching time for their children
+          </p>
+          <p class="font-weight-bold body-1">
+          Dedicated to my brave and beautiful daughter Aamani</p>      
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+};
+</script>
