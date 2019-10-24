77812f81025152514badafe50be1f400cd054a0d
Gaurav Shah <shahgaurav@yahoo.com>
2019-10-23 21:32:02 -0600

    Removed custom Firestore Jovo DB Integration file

--- alexa_skill/functions/src/app.js
+++ alexa_skill/functions/src/app.js
@@ -8,7 +8,7 @@ const { App } = require('jovo-framework');
 const { Alexa } = require('jovo-platform-alexa');
 const { GoogleAssistant } = require('jovo-platform-googleassistant');
 const { JovoDebugger } = require('jovo-plugin-debugger');
-const { Firestore } = require('./firestore');
+const { Firestore } = require('jovo-db-firestore');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
