7c8ae0aa9ddd9070c106f64e8fbfe9655341481c
Gaurav Shah <shahgaurav@yahoo.com>
2020-01-09 00:55:39 -0700

    Version 1.3.4 Disabled R8 in gradle.properties to prevent Android crash. Reverted to known good versions of all the dependencies.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/edocllc/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/edocllc/repos/patch_me/patch_me"
-export "FLUTTER_TARGET=/Users/edocllc/repos/patch_me/patch_me/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/edocllc/development/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.3.0"
-export "FLUTTER_BUILD_NUMBER=8"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_BUILD_NAME=1.3.4"
+export "FLUTTER_BUILD_NUMBER=12"
